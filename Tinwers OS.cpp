ee67316537f3365eb17439eaebbe6240db75108b
sunny blank <145170311+Tinwers@users.noreply.github.com>
2023-12-09 22:41:38 +0800

    Add files via upload

---
+++ Tinwers OS.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   system("title Tinwers mode selection [version:1.4.0]");
+   system("color a");
+   int ms=0;
+   cout<<"Tinwers OS mode selection[version:1.4.0]"<<endl;
+   cout<<"(c) SunnyBlank 保留所有权利。"<<endl<<endl;
+   cout<<"请选择模式  1. OS模式  2. 电脑病毒  3.Game  4.算法[1/2/3/4] ";
+   cin>>ms;
+   if(ms==1) system("start Tinwersprograms.exe");
+   if(ms==2) system("start Tinwersvirus.exe");
+   if(ms==3) system("start Tinwersgame.exe");
+   if(ms==4) system("start Tinwersarithmetic.exe");
+}
+
---
+++ Tinwersarithmetic.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int a;
+   system("title Tinwers OS arithmetic [version:1.4.0]");
+   cout<<"Tinwers OS arithmetic [version:1.4.0]"<<endl;
+   cout<<"(c) SunnyBlack 保留所有权利。"<<endl<<endl;
+   cout<<"1.深度优先遍利  2.贪心算法(暂时只支持这2个算法)  [1/2] ";
+   cin>>a;
+   cout<<endl;
+   if(a==1){
+      cout<<"1.exe(效果)  2.cpp(源码)  [1/2] ";
+      cin>>a;
+      if(a==1){
+      	system("start sdyxbianli.exe");
+	  }
+	  if(a==2){
+	  	system("start sdyxbianli.cpp");
+	  }
+   }
+   if(a==2){
+      cout<<"1.exe(效果)  2.cpp(源码)  [1/2] ";
+      cin>>a;
+      if(a==1){
+      	system("start txsf.exe");
+	  }
+	  if(a==2){
+	  	system("start txsf.cpp");
+	  }
+   }
+}
